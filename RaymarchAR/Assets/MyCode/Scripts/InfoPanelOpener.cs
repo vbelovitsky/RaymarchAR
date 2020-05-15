4587d3d890af86cdfe259cc043b2829ab15275ab
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2020-05-15 21:41:39 +0300

    add information panel and button

---
+++ RaymarchAR/Assets/MyCode/Scripts/InfoPanelOpener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanelOpener : MonoBehaviour
+{
+    public GameObject InfoPanel;
+    
+    public void TogglePanel()
+    {
+        if(InfoPanel != null)
+        {
+            InfoPanel.SetActive(!InfoPanel.activeSelf);
+        }
+    }
+
+}
---
+++ RaymarchAR/Assets/MyCode/Scripts/LinkOpener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkOpener : MonoBehaviour
+{
+
+    public string link = "https://github.com/vbelovitsky/RaymarchAR/tree/master/QR-codes";
+
+    public void OpenLink()
+    {
+        if(link != null)
+        {
+            Application.OpenURL(link);
+        }
+       
+    }
+}
